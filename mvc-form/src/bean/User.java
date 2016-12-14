183401a9abd2eb313394b0ff16e7242be2fe682f
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-12-15 00:18:36 +0200

    Add new project - MVC forms

---
+++ mvc-form/src/bean/User.java
@@ -0,0 +1,59 @@
+package bean;
+
+public class User {
+	private String email = "";
+	private String password = "";
+
+	// message that returned if the bean can't be validated
+	private String message = "";
+
+	public User() {
+	}
+
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public boolean validate() {
+		
+		if (email == null || email.length() == 0) {
+			message = "No email address set.";
+			return false;
+		} else if (!email.matches("\\w+@\\w+\\.\\w+")) {
+			message = "Invalid email address.";
+			return false;
+		}
+		
+		if (password == null || password.length() < 8) {
+			message = "Password must be at least 8 characters.";
+			return false;
+		} else if (password.matches("\\w*\\s+\\w*")) {
+			message = "Password cannot contain space.";
+			return false;
+		}
+		
+		return true;
+	}
+
+}
